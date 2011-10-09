b1e8ee0fe1df313e63f0cbbcd106235b02ad46d4
Pam Suebvisai <sinapam@proteus-tech.com>
2011-10-10 00:34:36 +0700

    add first unit tests

--- apps/barcamp_registration/tests/test_views.py
+++ apps/barcamp_registration/tests/test_views.py
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
+
+class TestRegistration(TestCase):
+    def test_get_form(self):
+        response = self.client.get('/barcamp_registration/')
+        self.assertIn(u'name="name"',response.content)
+        self.assertIn(u'name="email"',response.content)
+        self.assertIn(u'name="twitter"',response.content)
+        self.assertIn(u'name="website"',response.content)
+        self.assertIn(u'name="tshirt_size"',response.content)
+
+        self.assertEqual(response.content.count('required_mark'),2)
