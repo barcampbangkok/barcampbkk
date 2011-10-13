64b1b21ee27e53c74b385eea6931382c2330b7d2
Ches Martin <ches@nausicaamedia.com>
2011-10-13 22:24:19 +0700

    Give a hint about secrets.py upon ImportError

--- manage.py
+++ manage.py
@@ -13,7 +13,7 @@ from django.core.management import setup_environ, execute_from_command_line
 try:
     import settings as settings_mod # Assumed to be in the same directory.
 except ImportError:
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.stderr.write("Error: can't load settings -- there must be an ImportError with the settings module.\nHave you created settings/secrets.py?")
     sys.exit(1)
 
 # setup the environment before we start accessing things in the settings.
