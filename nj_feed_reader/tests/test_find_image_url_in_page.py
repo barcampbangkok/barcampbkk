2462e5bde1a9fa6877f8e17749067a550fc5f2dd
juacompe <juacompe@lufia.(none)>
2011-10-24 02:10:30 +0700

    fix the failing tests bcoz the url of twitpic and lockerz has been changed

--- nj_feed_reader/tests/test_find_image_url_in_page.py
+++ nj_feed_reader/tests/test_find_image_url_in_page.py
@@ -5,13 +5,13 @@ class TestFindImageUrlInPage(TestCase):
     def test_find_image_url_in_twitpic_page__found(self):
         page_url = 'http://twitpic.com/6m2t75'
         image_url = find_image_url_in_page(page_url)
-        expected_url = 'http://s1.proxy03.twitpic.com/photos/large/399879761.jpg'
+        expected_url = '//d3-05.twitpicproxy.com/photos/large/399879761.jpg'
         self.assertEqual(expected_url, image_url)
 
     def test_find_image_url_in_lockerz_page__found(self):
         page_url = 'http://lockerz.com/s/139778407'
         image_url = find_image_url_in_page(page_url)
-        expected_url = 'http://c0013959.cdn1.cloudfiles.rackspacecloud.com/x2_854d967'
+        expected_url = 'http://c0013959.r32.cf1.rackcdn.com/x2_854d967'
         self.assertEqual(expected_url, image_url)
 
     def test_find_image_url_in_yfrog_page__found(self):
